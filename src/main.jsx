be5d00fb35d149e2fdd1db70d9f404da9f1a2e61
Danie1T <82688216+Danie1T@users.noreply.github.com>
2024-09-04 01:22:32 -0400

    Feat: add OAuth handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import { ThemeProvider, createTheme } from '@mui/material'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const theme = createTheme({});
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </StrictMode>,
+  </ThemeProvider>
 )
